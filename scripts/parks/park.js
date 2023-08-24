8c3bc3bb2a5ce8bc2ee68fa7a47a6ba879a4bc31
jwgilliam <ggsmoustache@gmail.com>
2023-08-24 14:23:30 -0500

    func: added details and checkboxes

--- scripts/parks/park.js
+++ scripts/parks/park.js
@@ -5,12 +5,22 @@ const parkComponent = (park) => {
   return `
   <div class="park-card">
   <div class="park-name">${park.fullName}</div>
-  <image class="park-image" src="${park.images[0].url}" width="500" height="500">
+ 
   <div class="park-description">${park.description}</div>
   <button class="add-park--${park.id}">Add park to trip</button>
+  <details>
+  <summary>Data goes under here </summary>
+  <div>hereish</div>
+  <input type="checkbox" id="place-check" name="places" value="">
+  <label for="places-check">data goes here</label>
+  </details>
   </div>
   `
 }
 //export
 
 export default parkComponent
+
+// TODO: add back into html
+//TODO: Places data: .title and .bodyText
+//<image class="park-image" src="${park.images[0].url}" width="500" height="500"> 
